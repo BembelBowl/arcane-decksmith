b7fdb17998b2b7c7c4948fa58e6f0be41b2c10d3
Bembel Bowl <benjamin.ambros@gmail.com>
2026-09-15 12:41:18 +0200

    Add files via upload

---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,41 @@
+export default function AppFooter() {
+  return (
+    <footer>
+      Scryfall-Daten &amp; Bilder werden direkt von
+      Scryfall geladen. Keine Kaufentscheidung aufgrund von
+      Preisen. Turnierdaten: {" "}
+      <a
+        href="https://topdeck.gg"
+        target="_blank"
+        rel="noreferrer"
+      >
+        TopDeck.gg
+      </a>
+      . Commander-Communitydaten: {" "}
+      <a
+        href="https://edhrec.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        EDHREC
+      </a>
+      {" / "}
+      <a
+        href="https://archidekt.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Archidekt
+      </a>
+      . Combo-Daten: {" "}
+      <a
+        href="https://commanderspellbook.com"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Commander Spellbook
+      </a>
+      .
+    </footer>
+  );
+}
